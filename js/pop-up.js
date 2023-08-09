8a17a1cfbfda4cc5dd566c31486684dd7bf3a83e
SonoNessunk <91794916+SonoNessunk@users.noreply.github.com>
2023-08-09 06:23:32 +0200

    new navbar :)

--- js/pop-up.js
+++ js/pop-up.js
@@ -1,12 +1,27 @@
+// function openPopup(videoUrl) {
+//   var popup = document.getElementById('popup');
+//   var popupVideo = document.getElementById('popupVideo');
+//   popupVideo.src = videoUrl;
+//   popup.style.display = 'block';
+//   var windowHeight = window.innerHeight || document.documentElement.clientHeight;
+//   var popupHeight = popup.offsetHeight;
+//   var offset = (windowHeight - popupHeight) / 2;
+//   popup.style.top = offset + 'px';
+// }
+
+// function closePopup() {
+//   var popup = document.getElementById('popup');
+//   var popupVideo = document.getElementById('popupVideo');
+//   popupVideo.pause();
+//   popupVideo.currentTime = 0;
+//   popup.style.display = 'none';
+// }
+
 function openPopup(videoUrl) {
   var popup = document.getElementById('popup');
   var popupVideo = document.getElementById('popupVideo');
   popupVideo.src = videoUrl;
-  popup.style.display = 'block';
-  var windowHeight = window.innerHeight || document.documentElement.clientHeight;
-  var popupHeight = popup.offsetHeight;
-  var offset = (windowHeight - popupHeight) / 2;
-  popup.style.top = offset + 'px';
+  popup.style.display = 'flex';
 }
 
 function closePopup() {
