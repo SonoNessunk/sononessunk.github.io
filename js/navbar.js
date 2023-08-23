6180aa27e39e5f81453809a200df6f6eb6a890b1
SonoNessunk <91794916+SonoNessunk@users.noreply.github.com>
2023-08-23 21:25:14 +0200

    .

--- js/navbar.js
+++ js/navbar.js
@@ -1,27 +1,8 @@
 const body = document.querySelector("body"),
       nav = document.querySelector("nav"),
-      modeToggle = document.querySelector(".dark-light"),
       searchToggle = document.querySelector(".searchToggle"),
       sidebarOpen = document.querySelector(".sidebarOpen"),
       siderbarClose = document.querySelector(".siderbarClose");
-
-      let getMode = localStorage.getItem("mode");
-          if(getMode && getMode === "dark-mode"){
-            body.classList.add("dark");
-          }
-
-// js code to toggle dark and light mode
-      modeToggle.addEventListener("click" , () =>{
-        modeToggle.classList.toggle("active");
-        body.classList.toggle("dark");
-
-        // js code to keep user selected mode even page refresh or file reopen
-        if(!body.classList.contains("dark")){
-            localStorage.setItem("mode" , "light-mode");
-        }else{
-            localStorage.setItem("mode" , "dark-mode");
-        }
-      });
 
 // js code to toggle search box
         searchToggle.addEventListener("click" , () =>{
@@ -41,5 +22,3 @@ body.addEventListener("click" , e =>{
         nav.classList.remove("active");
     }
 });
-
-
