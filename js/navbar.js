95549b945342d6ee1b3aff0d81bf3dd0833c4c11
SonoNessunk <91794916+SonoNessunk@users.noreply.github.com>
2025-03-16 15:47:23 +0100

    fixes :)

--- js/navbar.js
+++ js/navbar.js
@@ -1,39 +1,41 @@
 const body = document.querySelector("body"),
-      nav = document.querySelector("nav"),
-      modeToggle = document.querySelector(".dark-light"),
-      sidebarOpen = document.querySelector(".sidebarOpen"),
-      siderbarClose = document.querySelector(".siderbarClose");
+  nav = document.querySelector("nav"),
+  modeToggle = document.querySelector(".dark-light"),
+  sidebarOpen = document.querySelector(".sidebarOpen"),
+  sidebarClose = document.querySelector(".sidebarClose");
 
-      let getMode = localStorage.getItem("mode");
-          if(getMode && getMode === "light-mode"){
-            body.classList.add("light");
-          }
+let getMode = localStorage.getItem("mode");
+if (getMode === "light-mode") {
+  body.classList.add("light");
+  modeToggle.classList.add("active");
+}
 
 // js code to toggle dark and light mode
-      modeToggle.addEventListener("click" , () =>{
-        modeToggle.classList.toggle("active");
-        body.classList.toggle("light");
+modeToggle.addEventListener("click", () => {
+  modeToggle.classList.toggle("active");
+  body.classList.toggle("light");
 
-        // js code to keep user selected mode even page refresh or file reopen
-        if(!body.classList.contains("light")){
-            localStorage.setItem("mode" , "dark-mode");
-        }else{
-            localStorage.setItem("mode" , "light-mode");
-        }
-      });
- 
-      
+  // js code to keep user selected mode even page refresh or file reopen
+  if (!body.classList.contains("light")) {
+    localStorage.setItem("mode", "dark-mode");
+  } else {
+    localStorage.setItem("mode", "light-mode");
+  }
+});
+
 //   js code to toggle sidebar
-sidebarOpen.addEventListener("click" , () =>{
-    nav.classList.add("active");
+sidebarOpen.addEventListener("click", () => {
+  nav.classList.add("active");
 });
 
-body.addEventListener("click" , e =>{
-    let clickedElm = e.target;
+body.addEventListener("click", (e) => {
+  let clickedElm = e.target;
 
-    if(!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")){
-        nav.classList.remove("active");
-    }
+  if (
+    !clickedElm.classList.contains("sidebarOpen") &&
+    !clickedElm.classList.contains("menu") &&
+    clickedElm.classList.contains("sidebarClose")
+  ) {
+    nav.classList.remove("active");
+  }
 });
-
-
