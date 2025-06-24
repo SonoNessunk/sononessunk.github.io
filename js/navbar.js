b973d131b3ea3289e27c5e396a91a968e7fb61d1
SonoNessunk <91794916+SonoNessunk@users.noreply.github.com>
2025-06-24 06:07:38 +0200

    Cleaned up codebase: - Rewrote CSS for better structure and clarity - Improved HTML layout - Temporarily dropped mobile support - Removed unused files (audio, old images, etc.)

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,6 @@
 const body = document.querySelector("body"),
   nav = document.querySelector("nav"),
-  modeToggle = document.querySelector(".dark-light"),
-  sidebarOpen = document.querySelector(".sidebarOpen"),
-  sidebarClose = document.querySelector(".sidebarClose");
+  modeToggle = document.querySelector(".theme-toggle");
 
 let getMode = localStorage.getItem("mode");
 if (getMode === "light-mode") {
@@ -20,22 +18,5 @@ modeToggle.addEventListener("click", () => {
     localStorage.setItem("mode", "dark-mode");
   } else {
     localStorage.setItem("mode", "light-mode");
-  }
-});
-
-//   js code to toggle sidebar
-sidebarOpen.addEventListener("click", () => {
-  nav.classList.add("active");
-});
-
-body.addEventListener("click", (e) => {
-  let clickedElm = e.target;
-
-  if (
-    !clickedElm.classList.contains("sidebarOpen") &&
-    !clickedElm.classList.contains("menu") &&
-    clickedElm.classList.contains("sidebarClose")
-  ) {
-    nav.classList.remove("active");
   }
 });
